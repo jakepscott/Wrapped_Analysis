63aa0753875d9b6257af5aa2d595f37742b1ec2a
jakepscott <jakepscott16@gmail.com>
2020-12-14 20:53:33 -0500

    Got full lyric and feature data for every song from Jan 2017 to Dec 2020

--- Get_Top_200_Data/Get_Top200_Feats_Lyrics.R
+++ Get_Top_200_Data/Get_Top200_Feats_Lyrics.R
@@ -9,6 +9,7 @@ library(rvest)
 library(remotes)
 library(genius)
 library(stringi)
+library(here)
 
 # Loading Necessary Data and Functions ------------------------------------
 load(here("Get_Top_200_Data/functions/keys"))
@@ -19,30 +20,32 @@ source("Get_Top_200_Data/functions/Playlist_Comparison_Function.R")
 
 
 # Load_Data ---------------------------------------------------------------
-data <- read_rds(here("Get_Top_200_Data/data/Jan_2017_Dec_2020.rds"))
+all_songs <- read_rds(here("Get_Top_200_Data/data/Jan_2017_Dec_2020.rds")) %>% #Getting consistent names
+  rename("Song"=Track_Name,"Id"=URI) 
+  
 data <- data %>% 
   #Playlists here are the years
   mutate(Playlist=year(date)) %>% 
-  #Getting consistent names
-  rename("Song"=Track_Name,
-         "Id"=URI) %>% 
   #Selecting only those columns I'd get from Tracks_Function
   select(Song,Id, Artist,"Artist_id"=artist_id,"Album"=album,"Album_id" = album_id,Playlist) %>% 
   distinct(Id,.keep_all = T)
 
 
 #Get Features
-Features <- Features_Function(track_data = data)
-saveRDS(Features,here("Get_Top_200_Data/data_new/Top200_Features.rds"))
+#Features <- Features_Function(track_data = data)
+#saveRDS(Features,here("Get_Top_200_Data/data_new/Top200_Features.rds"))
 #Get lyrics using function
-Lyrics <- Lyric_Generation_Function(tracks)
-saveRDS(Lyrics,here("Get_Top_200_Data/data_new/Top200_Lyrics.rds"))
+Lyrics <- Lyric_Generation_Function(data)
+#saveRDS(Lyrics,here("Get_Top_200_Data/data_new/Top200_Lyrics.rds"))
 
 
 #Getting features of the lyrics
 Lyric_Features <- Lyric_Analysis_Function(Lyrics) %>% select(-Lyrics)
+Lyric_Features <- Lyric_Features %>% distinct()
+#saveRDS(Lyric_Features,here("Get_Top_200_Data/data_new/Top200_Lyric_Features.rds"))
+
 
 #Joining
-Full_Data <- left_join(left_join(tracks,Features),Lyric_Features) %>% distinct()
-#saveRDS(Full_Data,here("data/Full_Data.rds"))
+Full_Data <-  all_songs %>% left_join(Lyrics) %>% left_join(Lyric_Features) 
+saveRDS(Full_Data,here("Get_Top_200_Data/data_new/Full_Top_200_Data.rds"))
 
